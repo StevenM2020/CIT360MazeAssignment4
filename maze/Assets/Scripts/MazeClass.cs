57aabfbb7222c9febc83077464030a6b57d5f5ee
Steven <steven.bikerider@gmail.com>
2021-10-28 00:57:01 -0400

    Finished Maze Generation
    I fixed some stuff with the generation and added the start and end of the maze.

--- maze/Assets/Scripts/MazeClass.cs
+++ maze/Assets/Scripts/MazeClass.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/* Program: MazeClass
+ * Author:  Steven Motz
+ * Purpose: This is used for making a maze object, holding all its data, and methods to modify that data.
+ * Date:    10/25/2021
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,40 +12,53 @@ public class MazeClass : MonoBehaviour
 {
 
     private int[,,] intMaze;
+    private int[] startLocation;
+    private int[] endLocation;
 
-        // Create a class constructor for the Car class
+        // Create a class constructor for the maze class
         public MazeClass(int x,int y,int z)
         {
+        // sizes the maze and adds room for walls
         intMaze = new int[x+2, y+2, z+2];
 
     }
 
+    // fills the maze
     public void generateMaze(int dencity)
     {
 
 
         System.Random rnd = new System.Random();
-        for ( int i = 0; i < intMaze.GetLength(0) ; i++)
+        for ( int i = 0; i < intMaze.GetLength(0) ; i++) // x
         {
-            for ( int j = 0; j < intMaze.GetLength(1); j++)
+            for ( int j = 0; j < intMaze.GetLength(1); j++) // y
             {
-                for (int k = 0; k < intMaze.GetLength(2); k++)
+                for (int k = 0; k < intMaze.GetLength(2); k++) // z
                 { 
-                    intMaze[i, j, k] = (j == 0 || j == intMaze.GetLength(1) - 1 ||
-                        k == 0 || k == intMaze.GetLength(2) - 1) ? 2 : (i == 0 || i == intMaze.GetLength(0) - 1) 
+                    /* chooses what number to put
+                    0 = empty
+                    1 = maze wall
+                    2 = outer wall
+                    3 = top or bottom
+                    4 = start
+                    5 = end
+                    */
+                    intMaze[i, j, k] = (i == 0 || i == intMaze.GetLength(0) - 1 ||
+                        k == 0 || k == intMaze.GetLength(2) - 1) ? 2 : (j == 0 || j == intMaze.GetLength(1) - 1) 
                         ? 3 : ((int)rnd.Next(100) > dencity ? 1 : 0);
                 }
             }
         }
     }
+    // converts the maze to a string for debugging
     public string getMazeAsString()
     {
         string str = "";
-        for (int i = 0; i < intMaze.GetLength(0); i++) {
+        for (int i = 0; i < intMaze.GetLength(0); i++) { //x
             str = str + "starting new level \n";
-            for (int j = 0; j < intMaze.GetLength(1); j++)
+            for (int j = 0; j < intMaze.GetLength(1); j++) //y
             {
-                for (int k = 0; k < intMaze.GetLength(2); k++)
+                for (int k = 0; k < intMaze.GetLength(2); k++) //z
                 {
                     str = str + intMaze[i, j, k];
                 }
@@ -49,16 +68,65 @@ public class MazeClass : MonoBehaviour
 
         return str;
     }
+    // checks if that spot in the maze is any kind of wall
     public bool isWall(int x, int y, int z)
     {
         return intMaze[x,y,z] == 1 || intMaze[x, y, z] == 2 || intMaze[x, y, z] == 3;
     }
+    // checks if that spot in the maze is an outer wall
     public bool isOuterWall(int x, int y, int z)
     {
         return intMaze[x, y, z] == 2;
     }
+    //checks if that spot in the maze is a top or bottom
     public bool isTopBottom(int x, int y, int z)
     {
         return intMaze[x, y, z] == 3;
+    }
+
+    //chooses that start and end point randomly
+    public void pickStartandEnd()
+    {
+        bool setStart = false;
+        bool setEnd = false;
+        System.Random rnd = new System.Random();
+
+        // repeats until it finds and empty spot
+        while (!setStart)
+        {
+            int x = rnd.Next(intMaze.GetLength(0) -1), y = rnd.Next(intMaze.GetLength(1) - 1), z = rnd.Next(intMaze.GetLength(2) - 1);
+            if(intMaze[x,y,z] == 0)
+            {
+                setStart = true;
+                startLocation = new int[3] {x,y,z};
+                intMaze[x, y, z] = 4;
+
+            }
+            Debug.Log(string.Join("start ", x, y, z));
+
+        }
+        // repeats until it finds and empty spot
+        while (!setEnd)
+        {
+            int x = rnd.Next(intMaze.GetLength(0) - 1), y = rnd.Next(intMaze.GetLength(1) - 1), z = rnd.Next(intMaze.GetLength(2) - 1);
+            if (intMaze[x, y, z] == 0)
+            {
+                setEnd = true;
+                endLocation = new int[3] { x, y, z };
+                intMaze[x, y, z] = 5;
+            }
+            Debug.Log(string.Join("end ", x, y, z));
+        }
+    }
+
+    // gets start cords
+    public int[] getStartLocation()
+    {
+        return startLocation;
+    }
+    // gets end cords
+    public int[] getEndLocation()
+    {
+        return endLocation;
     }
 }
--- maze/Assets/Scripts/MazeController.cs
+++ maze/Assets/Scripts/MazeController.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/* Program: MazeController
+ * Author:  Steven Motz
+ * Purpose: This program creates a maze object, gives it information from the user,
+ *          shows the maze in the Unity editor, and runs the searches.
+ * Date:    10/25/2021
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,25 +14,44 @@ public class MazeController : MonoBehaviour
     public Material cubeMaterial;
     public Material outerwallMaterial;
     public Material topOrBottomMaterial;
+    public Material searchMaterial;
+    public Material startEndMaterial;
 
-    private int intX = 50, intY = 1, intZ = 50;
+    public int dencity;
+
+    public int intX = 50, intY = 1, intZ = 30;
     MazeClass maze;
 
     // Start is called before the first frame update
     void Start()
     {
         maze = new MazeClass(intX, intY, intZ);
-        maze.generateMaze(80);
+        maze.generateMaze(dencity);
        Debug.Log( maze.getMazeAsString());
         showMaze();
+        maze.pickStartandEnd();
+        showStartEnd();
     }
 
-    // Update is called once per frame
-    void Update()
+    // this function controls the start and end
+    void showStartEnd()
     {
+        int[] startLocation = maze.getStartLocation();
+        int[] endLocation = maze.getEndLocation();
         
+        // makes start cube
+        GameObject startCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        startCube.transform.position = new Vector3(startLocation[0], startLocation[1], startLocation[2]);
+        startCube.GetComponent<Renderer>().material = startEndMaterial;
+
+        // makes end cube
+        GameObject endCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        endCube.transform.position = new Vector3(endLocation[0], endLocation[1], endLocation[2]);
+        endCube.GetComponent<Renderer>().material = startEndMaterial;
+
     }
 
+    // makes the maze cubes and applies materails to them.
     void showMaze()
     {
         for (int i = 0; i < intX+2; i++)
@@ -36,7 +62,7 @@ public class MazeController : MonoBehaviour
                 {
                     if (maze.isWall(i, j, k))
                     {
-
+                       
                         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                         cube.transform.position = new Vector3(i, j, k);
                         if (maze.isOuterWall(i, j, k))
