1a06e031b3f5ca8749460c44ddf4c8a6fad7bb73
Ethan Lehutsky <etl4@pct.edu>
2021-10-29 19:01:24 -0400

    The GUI and basic Script
    The GUI and a Handler needs camera adjustments

---
+++ maze/Assets/Scripts/EventController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/*
+ *Authors: Ethan Lehutsky and Steven Motz
+ *Purpose: A event handler that links the methods from Maze controller to the GUI interface
+ */
+
+public class EventController : MonoBehaviour
+{
+    public void CreateMaze()
+    {
+
+    }
+    
+    public void StartEnd()
+    {
+
+    }
+    
+    public void BFS()
+    {
+
+    }
+    public void DFS()
+    {
+
+    }
+
+    public void doRestart()
+    {
+
+    }
+
+
+
+}
