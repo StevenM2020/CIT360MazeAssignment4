2680cb5da3860c4567dbfe4706db9ec438027e51
Steven786 <steven.bikerider@gmail.com>
2021-10-30 22:49:52 -0400

    DFS Finished
    Created DFS function
    Created DFS Maze
    Created DFS Maze function in maze class
    DFS now draws a path

--- maze/Assets/Scripts/MazeClass.cs
+++ maze/Assets/Scripts/MazeClass.cs
@@ -14,12 +14,14 @@ public class MazeClass : MonoBehaviour
     private int[,,] intMaze;
     private int[] startLocation;
     private int[] endLocation;
+    private int[,,] DFSMaze;
 
         // Create a class constructor for the maze class
         public MazeClass(int x,int y,int z)
         {
         // sizes the maze and adds room for walls
         intMaze = new int[x+2, y+2, z+2];
+        DFSMaze = new int[x+2,y+2, z+2];
 
     }
 
@@ -46,6 +48,7 @@ public class MazeClass : MonoBehaviour
                     intMaze[i, j, k] = (i == 0 || i == intMaze.GetLength(0) - 1 ||
                         k == 0 || k == intMaze.GetLength(2) - 1) ? 2 : (j == 0 || j == intMaze.GetLength(1) - 1) 
                         ? 3 : ((int)rnd.Next(100) > dencity ? 1 : 0);
+                    DFSMaze[i, j, k] = (intMaze[i, j, k] == 1 || intMaze[i, j, k] == 2 || intMaze[i, j, k] == 3 ? 1 : 0);
                 }
             }
         }
@@ -100,6 +103,7 @@ public class MazeClass : MonoBehaviour
                 setStart = true;
                 startLocation = new int[3] {x,y,z};
                 intMaze[x, y, z] = 4;
+                DFSMaze[x, y, z] = 4;
 
             }
 
@@ -113,6 +117,7 @@ public class MazeClass : MonoBehaviour
                 setEnd = true;
                 endLocation = new int[3] { x, y, z };
                 intMaze[x, y, z] = 5;
+                //DFSMaze[x, y, z] = 5;
             }
         }
     }
@@ -126,5 +131,38 @@ public class MazeClass : MonoBehaviour
     public int[] getEndLocation()
     {
         return endLocation;
+    }
+    public int[] DFSMove(int x,int y,int z)
+    {
+        if(DFSMaze[x+1,y,z] == 0)
+        {
+            DFSMaze[x+1,y,z] = 1;
+            return new int[3] { x+1, y, z };
+        }
+        else if(DFSMaze[x-1, y, z] == 0){
+            DFSMaze[x-1, y, z] = 1;
+            return new int[3] { x-1, y, z };
+        }
+        else if (DFSMaze[x, y+1, z] == 0){
+            DFSMaze[x, y+1, z] = 1;
+            return new int[3] { x, y+1, z };
+        }
+        else if (DFSMaze[x, y-1, z] == 0){
+            DFSMaze[x, y-1, z] = 1;
+            return new int[3] { x, y-1, z };
+        }
+        else if (DFSMaze[x, y, z+1] == 0){
+            DFSMaze[x, y, z+1] = 1;
+            return new int[3] { x, y, z+1 };
+        }
+        else if (DFSMaze[x, y, z-1] == 0){
+            DFSMaze[x, y, z-1] = 1;
+            return new int[3] { x, y, z-1 };
+        }
+        else
+        {
+            return new int[3] { x, y, z };
+        }
+
     }
 }
--- maze/Assets/Scripts/MazeController.cs
+++ maze/Assets/Scripts/MazeController.cs
@@ -8,6 +8,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using System.Linq;
 
 public class MazeController : MonoBehaviour
 {
@@ -21,16 +23,79 @@ public class MazeController : MonoBehaviour
 
     public int intX = 50, intY = 1, intZ = 30;
     MazeClass maze;
-
+    public int intCount = 0;
     // Start is called before the first frame update
     void Start()
     {
+        int[] test1 = new int[3] { 1, 2, 3 };
+        int[] test2 = new int[3] { 1, 2, 3 };
+        int[] test3 = new int[3] { 1, 2, 0 };
+
         maze = new MazeClass(intX, intY, intZ);
         maze.generateMaze(dencity);
-       Debug.Log( maze.getMazeAsString());
         showMaze();
         maze.pickStartandEnd();
         showStartEnd();
+
+        Stack<int[]> stackDFS = new Stack<int[]>();
+        stackDFS.Push(maze.getStartLocation());
+        stackDFS = DFS(stackDFS);
+        Debug.Log("stack size: " + stackDFS.Count + "call level: " + intCount);
+        if (((int[])stackDFS.Peek()).SequenceEqual(maze.getEndLocation()))
+        {
+            int count = stackDFS.Count;
+            Debug.Log("END, LETS GO!!!!!");
+            for (int i = 0; i < count; i++)
+            {
+                int[] nextLoacion = new int[3];
+                nextLoacion = (int[])stackDFS.Pop();
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(nextLoacion[0], nextLoacion[1], nextLoacion[2]);
+                cube.GetComponent<Renderer>().material = searchMaterial;
+                cube.name = "DFS Search Cube";
+                cube.isStatic = true;
+                
+            }
+        }
+        else if (((int[])stackDFS.Peek()).SequenceEqual(maze.getStartLocation()))
+        {
+            Debug.Log("Start, so close !!!!!");
+        }
+        else
+        {
+            Debug.Log("WHYYYYYY!!!!!");
+        }
+
+    }
+    public Stack<int[]> DFS(Stack<int[]> stackDFS)
+    {
+        intCount++;
+        Debug.Log("call level: " + intCount);
+        int[] nextLoacion = new int[3];
+       nextLoacion = maze.DFSMove(((int[])stackDFS.Peek())[0], ((int[])stackDFS.Peek())[1], ((int[])stackDFS.Peek())[2]);
+        if (maze.getEndLocation().SequenceEqual((int[])stackDFS.Peek()))
+        {
+            return stackDFS;
+        }
+        else if(nextLoacion.SequenceEqual((int[])stackDFS.Peek()) && !maze.getStartLocation().SequenceEqual((int[])stackDFS.Peek()))
+        {
+            stackDFS.Pop();
+            return DFS(stackDFS);
+        }else if (!nextLoacion.SequenceEqual((int[])stackDFS.Peek()))
+        {
+            stackDFS.Push(nextLoacion);
+            return DFS(stackDFS);
+        }
+        else if (maze.getStartLocation().SequenceEqual((int[])stackDFS.Peek()))
+        {
+            return stackDFS;
+        }
+        else
+        {
+            return stackDFS;
+        }
+        
+ 
     }
 
     // this function controls the start and end
@@ -43,17 +108,24 @@ public class MazeController : MonoBehaviour
         GameObject startCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         startCube.transform.position = new Vector3(startLocation[0], startLocation[1], startLocation[2]);
         startCube.GetComponent<Renderer>().material = startEndMaterial;
-
+        startCube.name = "Start";
         // makes end cube
         GameObject endCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         endCube.transform.position = new Vector3(endLocation[0], endLocation[1], endLocation[2]);
         endCube.GetComponent<Renderer>().material = startEndMaterial;
-
+        endCube.name = "End";
+    }
+    void makecube(int[] location)
+    {
+        GameObject startCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        startCube.transform.position = new Vector3(location[0], location[1], location[2]);
+        startCube.GetComponent<Renderer>().material = searchMaterial;
     }
 
     // makes the maze cubes and applies materails to them.
     void showMaze()
     {
+        int cubeCount = 0;
         for (int i = 0; i < intX+2; i++)
         {
             for (var j = 0; j < intY+2; j++)
@@ -65,6 +137,7 @@ public class MazeController : MonoBehaviour
                        
                         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                         cube.transform.position = new Vector3(i, j, k);
+                        
                         if (maze.isOuterWall(i, j, k))
                         {
                             cube.GetComponent<Renderer>().material = outerwallMaterial;
@@ -77,6 +150,9 @@ public class MazeController : MonoBehaviour
                         {
                             cube.GetComponent<Renderer>().material = cubeMaterial;
                         }
+                        cubeCount++;
+                        cube.name = "cube" + cubeCount;
+                        cube.isStatic = true;
                         
                     }
                 }
